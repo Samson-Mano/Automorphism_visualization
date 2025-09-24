0a96abd41db23f237008d71c62f4b6a27b93a3dc
Mano Samson <saminnx@gmail.com>
2025-09-24 02:33:24 -0300

    Add project files.

---
+++ Automorphism_visualization/main_frm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Automorphism_visualization
+{
+    partial class main_frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_frm));
+            this.SuspendLayout();
+            // 
+            // main_frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 631);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main_frm";
+            this.Text = "Automorphism of unit disk visualization";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
